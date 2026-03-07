d42e69b0527eb657c6f619dd4da4e1467ab25322
Triple Que <tripleq@gmail.com>
2026-03-06 21:20:09 -0500

    Configure custom domain and fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/can-retire/',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
